8f05182fe0dc81feb8ae58bfd36421926e326d4f
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-03-29 20:02:46 +0900

    [Update]: add ImageMessageInputValidator

--- SOLID_sample/Single_Responsibility_Principle/SingleResposibilityPrinciple.swift
+++ SOLID_sample/Single_Responsibility_Principle/SingleResposibilityPrinciple.swift
@@ -5,14 +5,27 @@ final class CommonMessageAPI {
     func sendImageMessage(image: UIImage, text: String?, completion: @escaping (ImageMessage?) -> Void) { ... }
 }
 
+// ImageMessageの入力値に関するプロパティはimageとtextの２つだとわかる↓
+struct ImageMessageInputValidator {
+    let image: UIImage?
+    let text: String?
+    var isValid: Bool {
+        if image == nil { return false }
+        if let text = text, text.count > 80 { return false }
+        return true
+    }
+}
+
+// validationのみを責務とする型 → MessageSender の isLoading や result といった状態のことは気にする必要はない
 struct MessageInputValidator {
     let messageType: MessageType
     let image: UIImage?
     let text: String?
     private var isTextValid: Bool {
-        switch messageType {
+        switch messageType { // ← ここでswitchする必要がない(TextMessageとImageMessageは本来関連のない処理のはず)
         case .text: return text != nil && text!.count <= 300 // 300 字以内
-        case .image: return text == nil || text!.count <= 80 // 80 字以内 or nil case .official: return false // OfficialMessageはありえない
+        case .image: return text == nil || text!.count <= 80 // 80 字以内 or nil
+        case .official: return false // OfficialMessageはありえない
         }
     }
     
